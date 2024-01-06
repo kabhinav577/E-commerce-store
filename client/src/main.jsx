75ed31428a5dda544db14a179ab008bcba026761
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-06 19:17:54 +0530

    feat: create state using reduxjs/toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,23 @@ import './index.css';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './theme';
 import { CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './state';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,5 +1,93 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Badge, Box, IconButton } from '@mui/material';
+import {
+  PersonOutline,
+  ShoppingCartOutlined,
+  MenuOutlined,
+  SearchOutlined,
+} from '@mui/icons-material';
+import { shades } from '../../theme';
+import { useNavigate } from 'react-router-dom';
+import { setIsCartOpen } from '../../state';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  const dispacth = useDispatch();
+
+  const cart = useSelector((state) => state.cart.cart);
+  // console.log(cart);
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgba(255, 255, 255, 0.95)"
+      color="black"
+      position="fixed"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          onClick={() => navigate('/')}
+          sx={{
+            '&:hover': {
+              cursor: 'pointer',
+            },
+          }}
+          color={shades.secondary[500]}
+        >
+          TechMart
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2"
+        >
+          <IconButton sx={{ color: 'black' }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: 'black' }}>
+            <PersonOutline />
+          </IconButton>
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              '& .MuiBadge-badge': {
+                right: 5,
+                top: 5,
+                padding: '0 4px',
+                height: '14px',
+                minWidth: '13px',
+              },
+            }}
+          >
+            <IconButton
+              onClick={() => dispacth(setIsCartOpen({}))}
+              sx={{ color: 'black' }}
+            >
+              <ShoppingCartOutlined />
+            </IconButton>
+          </Badge>
+          <IconButton sx={{ color: 'black' }}>
+            <MenuOutlined />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Navbar;
