36198768dd7c1f85782d1cdf07bbcdb0f906a5fd
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-07 19:41:20 +0530

    feat: working on Carousel comp and import all img

---
+++ client/src/assets/imageData.js
@@ -0,0 +1,13 @@
+import Imac from '../assets/imac-carousel.jpg';
+import Iphone from '../assets/iphone-carousel.png';
+import Ipad from '../assets/ipad-carousel.png';
+import Iwatch from '../assets/iwatch-carousel.png';
+import TitaniumIphone from '../assets/titanium-carousel.png';
+
+export const imageData = {
+  Iphone,
+  Imac,
+  Ipad,
+  Iwatch,
+  TitaniumIphone,
+};
